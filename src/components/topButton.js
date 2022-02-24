cad121a67e60d80ee8fbca3824ab2a2c0a24237c
Adam Beatty <beattyad@tcd.ie>
2022-02-24 10:45:18 +0000

    created file for top button

---
+++ src/components/topButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function topButton() {
+  function Nextlocation(){
+
+  }
+
+  return (
+    <div>topButton
+        <button onClick={Nextlocation}>button1</button>
+        <button>button2</button>
+        <button>button3</button>
+    </div>
+  )
+}
