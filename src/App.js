8625eb9ae80eb53258960d69690b95abfa5edfb1
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-03-08 18:22:33 +0000

    Setup app to test video player.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css'
 import { useState, useRef, useEffect } from 'react'
+import VideoPlayer from "./VideoPlayer.jsx"
 
 function App() {
 	const [file, setFile] = useState("");
@@ -12,6 +13,7 @@ function App() {
   	<div className="App">
 		  <h1>Electron Application</h1>
 		  <input type="file" onChange={(e) => {setFile(e.target.files[0])}}></input>
+		<VideoPlayer videoSrc="file:///Users/harvey/TestCard.mp4"></VideoPlayer>
   	</div>
   );
 }
