37c72c314e3ec3a5a5fe92426b8166f3dd243089
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-03-10 21:41:53 +0000

    Canvas resizes based on video original height and width.

--- src/CanvasComponent.jsx
+++ src/CanvasComponent.jsx
@@ -7,16 +7,35 @@ function CanvasComponent(props) {
 	// 0 == Box, 1 == Polygon, 2 == Freehand
 	const [currentTool, setCurrentTool] = useState("");
 
+	// Create refs to access video and canvas elements.
+	const videoElement = useRef(null);
+	const canvasElement = useRef(null);
+
+	// Create variables used to change canvas size.
+	const [canvasWidth, setCanvasWidth] = useState(0);
+	const [canvasHeight, setCanvasHeight] = useState(0);
+
+	// Function that will run once the video is loaded and resize the canvas to match.
 	const resizeCanvas = () => {
-		console.log("RESIZE");
+		// Get video dimensions.
+		const width = videoElement.current.videoWidth;
+		const height = videoElement.current.videoHeight;
+
+		console.log(width, height);
+
+		// Resize canvas.
+		setCanvasWidth(width);
+		setCanvasHeight(height);
+		
+		console.log(canvasWidth, canvasHeight);
 	}
 
 	return (
 		<div className="CanvasComponent">
-			<video className="video" onLoadedData={() => resizeCanvas()} autoPlay controls>
-				<source src={props.videoSrc} type="video/mp4"/>
+			<video ref={videoElement} className="video" onLoadedData={() => resizeCanvas()} autoPlay>
+				<source ref={canvasElement} src={props.videoSrc} type="video/mp4"/>
 			</video>
-			<canvas className="canvas"/>
+			<canvas style={{width: canvasWidth+"px", height: canvasHeight+"px"}} className="canvas"/>
 		</div>
 	);
 }
