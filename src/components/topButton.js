f7e94f40b5848e8c26c0d7542582f6ee9f2ae606
Adam Beatty <beattyad@tcd.ie>
2022-03-10 15:39:39 +0000

    style for top button

--- src/components/topButton.js
+++ src/components/topButton.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import styled from "styled-components";
 
-export default function topButton() {
-  function Nextlocation(){
 
-  }
+const Button = styled.button`
+background-color : #3949ab;
+color: white;
+padding: 5px 15px;
+border-radius: 5px;
+height: 60px;
+width: 280px;
+text-transform: uppercase;
+cursor: pointer;
+outline: 0;
+transition: ease background-color 250ms;
+margin: 20px 70px;
+&:hover{
+background-color: #283593;
+} `
 
-  return (
-    <div>topButton
-        <button onClick={Nextlocation}>button1</button>
-        <button>button2</button>
-        <button>button3</button>
-    </div>
-  )
+function uploadFile(){
+	document.getElementById("selectFile").click()
+	}
+
+function Buttons(){
+	
+  return <>
+<Button>new annotation</Button>
+<Button>Import existing annotation</Button>
+<Button onClick={uploadFile.bind(this)}>add video</Button>
+<input id ="selectFile"type={"file"} style={{display:"none"}}></input>
+</>
 }
+export default Buttons
+
