64d042758909e74ab6d3fd00fe090c797b971da6
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-03-10 23:27:15 +0000

    Added function to get percentage value of a click position on the canvas.

--- src/CanvasComponent.jsx
+++ src/CanvasComponent.jsx
@@ -3,17 +3,45 @@ import { useState, useRef } from 'react'
 import "./styles/Canvas.css"
 
 function CanvasComponent(props) {
-	// currentTool stores which tool is currently being used.
+
+	// Stores which tool is currently being used.
 	// 0 == Box, 1 == Polygon, 2 == Freehand
-	const [currentTool, setCurrentTool] = useState("");
+	const currentTool = 0;
+
+	// Stores if we are currently drawing a bounding box
+	let drawingBox = false;
+	// Stores the initial coordinate of bounding box
+	let initialX, initialY;
+
+
+	// Create variables used to change canvas size.
+	const [canvasWidth, setCanvasWidth] = useState(0);
+	const [canvasHeight, setCanvasHeight] = useState(0);
 
 	// Create refs to access video and canvas elements.
 	const videoElement = useRef(null);
 	const canvasElement = useRef(null);
 
-	// Create variables used to change canvas size.
-	const [canvasWidth, setCanvasWidth] = useState(0);
-	const [canvasHeight, setCanvasHeight] = useState(0);
+	// Function that converts client( Relative to viewpoint ) coordinates to percentage of the video.
+	const coordsToPercent = (x, y) => {
+		// Get top left position of video
+		const videoTop = videoElement.current.getBoundingClientRect().top;
+		const videoLeft = videoElement.current.getBoundingClientRect().left;
+
+		// Get video element size
+		const videoElementWidth = videoElement.current.getBoundingClientRect().width;
+		const videoElementHeight = videoElement.current.getBoundingClientRect().height;
+
+		// Subtract video position from coords to get position inside video element
+		const videoX = x-videoLeft;
+		const videoY = y-videoTop;
+
+		// Get X Y as percentage of video
+		const xPercentage = videoX/videoElementWidth;
+		const yPercentage = videoY/videoElementHeight;
+
+		return [xPercentage, yPercentage];
+	}
 
 	// Function that will run once the video is loaded and resize the canvas to match.
 	const resizeCanvas = () => {
@@ -28,12 +56,22 @@ function CanvasComponent(props) {
 		setCanvasHeight(height);
 	}
 
+	// Function that runs once mouse is clicked, Used for drawing annotations
+	const mouseDown = (e) => {
+		drawingBox = true;
+		const clientX = e.clientX;
+		const clientY = e.clientY;
+		console.log(clientX, clientY);	
+		const [xPercent, yPercent] = coordsToPercent(clientX, clientY);
+		console.log(xPercent, yPercent);
+	}
+
 	return (
 		<div className="CanvasComponent">
 			<video ref={videoElement} className="video" onLoadedData={() => resizeCanvas()} autoPlay>
 				<source ref={canvasElement} src={props.videoSrc} type="video/mp4"/>
 			</video>
-			<canvas style={{width: canvasWidth+"px", height: canvasHeight+"px"}} className="canvas"/>
+			<canvas onMouseDown={(e) => mouseDown(e)} style={{width: canvasWidth+"px", height: canvasHeight+"px"}} className="canvas"/>
 		</div>
 	);
 }
