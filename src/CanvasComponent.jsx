85632b472887f845d605dcfb0c9e1768e3f4564e
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-03-10 20:07:50 +0000

    Changed to function components.

--- src/CanvasComponent.jsx
+++ src/CanvasComponent.jsx
@@ -2,30 +2,23 @@ import React from 'react';
 import { useState, useRef } from 'react'
 import "./styles/Canvas.css"
 
-class CanvasComponent extends React.Component {
+function CanvasComponent(props) {
 	// currentTool stores which tool is currently being used.
 	// 0 == Box, 1 == Polygon, 2 == Freehand
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentTool: 0,
-		}
+	const [currentTool, setCurrentTool] = useState("");
+
+	const resizeCanvas = () => {
+		console.log("RESIZE");
 	}
 
-	resizeCanvas() {
-		console.log(this.refs.video);
-	}
-
-	render() {
-		return (
-			<div className="CanvasComponent">
-				<video ref="video" className="video" onLoadedData={() => this.resizeCanvas()} autoPlay controls>
-					<source src={this.props.videoSrc} type="video/mp4"/>
-				</video>
-				<canvas ref="canvas" className="canvas"/>
-			</div>
-		);
-	}
+	return (
+		<div className="CanvasComponent">
+			<video className="video" onLoadedData={() => resizeCanvas()} autoPlay controls>
+				<source src={props.videoSrc} type="video/mp4"/>
+			</video>
+			<canvas className="canvas"/>
+		</div>
+	);
 }
 
 export default CanvasComponent;
