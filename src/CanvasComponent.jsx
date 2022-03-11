11eecba9626fc4b8c383a92e6b39bb70bd62449d
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-03-11 00:12:01 +0000

    Added behaviour allowing you to start drawing a bounding box. Currently have an error with coordinates.

--- src/CanvasComponent.jsx
+++ src/CanvasComponent.jsx
@@ -12,6 +12,8 @@ function CanvasComponent(props) {
 	let drawingBox = false;
 	// Stores the initial coordinate of bounding box
 	let initialX, initialY;
+	// Stores the context for drawing the box as the user is drawing it
+	let currentContext;
 
 
 	// Create variables used to change canvas size.
@@ -43,6 +45,15 @@ function CanvasComponent(props) {
 		return [xPercentage, yPercentage];
 	}
 
+	// Function that converts coordinates( In percentage of video form ) to pixels on canvas for drawing.
+	const percentToCanvasPixels = (xPercent, yPercent) => {
+		// Convert percentages to pixels.
+		const xPixels = canvasWidth*xPercent;
+		const yPixels = canvasHeight*yPercent;
+
+		return [xPixels, yPixels];
+	}
+
 	// Function that will run once the video is loaded and resize the canvas to match.
 	const resizeCanvas = () => {
 		// Get video dimensions.
@@ -56,22 +67,58 @@ function CanvasComponent(props) {
 		setCanvasHeight(height);
 	}
 
+	// Function that gets a new context for our canvas
+	const getContext = () => {
+		return canvasElement.current.getContext('2d');	
+	}
+
 	// Function that runs once mouse is clicked, Used for drawing annotations
 	const mouseDown = (e) => {
+		// Start drawing new box
 		drawingBox = true;
+		// Record starting X and Y
 		const clientX = e.clientX;
 		const clientY = e.clientY;
-		console.log(clientX, clientY);	
+		// Convert X and Y to percentage
 		const [xPercent, yPercent] = coordsToPercent(clientX, clientY);
-		console.log(xPercent, yPercent);
+		// Store starting position
+		initialX = xPercent;
+		initialY = yPercent;
+		const [startX, startY] = percentToCanvasPixels(initialX, initialY);
+		console.log(clientX, clientY);
+		console.log(startX, startY);
+		// Create new context.
+		currentContext = getContext();
+	}
+
+	const mouseMove = (e) => {
+		// Get the current mouse position.
+		const clientX = e.clientX;
+		const clientY = e.clientY;
+		// Convert to percentage of canvas.
+		const [xPercent, yPercent] = coordsToPercent(clientX, clientY);
+		// Convert to canvas position.
+		const [endX, endY] = percentToCanvasPixels(xPercent, yPercent);
+
+		// Check if we are currently drawing a box
+		if(drawingBox) {
+			// Clear the last frame of drawing.
+			currentContext.clearRect(0, 0, 
+				canvasElement.current.getBoundingClientRect().width, canvasElement.current.getBoundingClientRect().height);
+			// Convert the starting X Y percentages into coordinates for drawing.
+			const [startX, startY] = percentToCanvasPixels(initialX, initialY);
+			console.log(startX, startY);
+			// Draw our current box.
+			currentContext.strokeRect(startX, startY, endX-startX, endY-startY);
+		}
 	}
 
 	return (
 		<div className="CanvasComponent">
 			<video ref={videoElement} className="video" onLoadedData={() => resizeCanvas()} autoPlay>
-				<source ref={canvasElement} src={props.videoSrc} type="video/mp4"/>
+				<source src={props.videoSrc} type="video/mp4"/>
 			</video>
-			<canvas onMouseDown={(e) => mouseDown(e)} style={{width: canvasWidth+"px", height: canvasHeight+"px"}} className="canvas"/>
+			<canvas ref={canvasElement} onMouseDown={(e) => mouseDown(e)} onMouseMove={(e) => mouseMove(e)} style={{width: canvasWidth+"px", height: canvasHeight+"px"}} className="canvas"/>
 		</div>
 	);
 }
