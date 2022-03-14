b8b36f7ee96c58e87099677e9a2dc648e3d8ba8b
NickDempseyDev <nickdempseydev@gmail.com>
2022-03-14 10:55:31 +0000

    preparing demo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   	<div className="App">
 		  <h1>Electron Application</h1>
 		  <input type="file" onChange={(e) => {setFile(e.target.files[0])}}></input>
-		<CanvasComponent videoSrc="file:///Users/harvey/TestCard.mp4"></CanvasComponent>
+		<CanvasComponent videoSrc="C:\Users\nickd\Videos\Scrum4Group9.mp4"></CanvasComponent>
   	</div>
   );
 }
