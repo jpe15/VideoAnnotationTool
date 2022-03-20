eba5dcc2db2a3c09af8462bf95eaf9356c8dc5a4
Adam Beatty <beattyad@tcd.ie>
2022-03-20 11:03:05 +0000

    updated import of metadata

--- public/import.js
+++ public/import.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import Data from './data.json' //this comes from the user.
 export const importannoation = () => {
   return (
-    <div>import</div>
+    <div>importannoation
+        <div className="posts">
+            {Data.map(post=>{
+                return (
+                    <>
+                    <h4>{post.name}</h4>
+                    <p>{"post.videopath"}</p>
+                    <p>{"post.imagepath"}</p>
+                    </>
+                )
+            })
+        }
+        </div>
+    </div>
   )
 }
