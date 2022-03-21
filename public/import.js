ea1cedd2e64b11891888cfe0ad0d563a852c317c
Adam Beatty <beattyad@tcd.ie>
2022-03-21 18:10:17 +0000

    updated import

--- public/import.js
+++ public/import.js
@@ -1,16 +1,9 @@
-"use strict";
 function importannoataions(filePath){
-fetch('./data.json')
+fetch('./filePath.json')
 .then(function(resp){
     return resp.json()
 })
-.then(function importannoataions(data){
-    console.log(data.metadata.name)
-    console.log(data.metadata.videopath)
-    console.log(data.metadata.imagepath)
-    console.log(data.annotations)
-})
 }
-module.exports= importannoataions;
+module.exports= importannoatations;
     
 
