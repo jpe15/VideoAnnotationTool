370e77a334809b8d00da7f07b241f2de14fa6494
Maham Fatima <mfatima@tcd.ie>
2022-03-21 16:45:18 +0000

    styling imported from annotation.css into the annotationsBar.jsx

--- src/components/AnnotationsBar.jsx
+++ src/components/AnnotationsBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Annotations from "./Annotations";
 import {useAnnotations} from "./AppContext";
+import "../styles/Annotations.css";
 
 const AnnotationsBar = () => {
     const [annotations, setAnnotations] = useAnnotations();
     return (
         <div>
+            <div className="blackBar"></div>
             {annotations?.map((annotations) => {return <Annotations annotations={annotations}/>})}
         </div>
     );
