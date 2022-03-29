aebc68fc6acc572f5b5edf17f5c2156c2d01e3ae
Maham Fatima <mfatima@tcd.ie>
2022-03-29 16:35:25 +0100

    Label and comment input field styled to match the background colour

--- src/components/Annotations.jsx
+++ src/components/Annotations.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {useAnnotations} from "./AppContext";
 import "../styles/Annotations.css";
 
+
 const Annotations = ({annotation, index}) =>{
     const [annotations, setAnnotations] = useAnnotations();
     useEffect(() => {
@@ -21,10 +22,10 @@ const updateComment =  (comment) => {
 return (
     <div>
         <h3 style = {{color: "white"}}>
-            Label: <input type = "text" placeholder="Enter label" value = {annotation?.label} onChange = {(e) => updateLabel(e.target.value)}/></h3>
+            Label: < input type = "text"   placeholder="Enter label" value = {annotation?.label} onChange = {(e) => updateLabel(e.target.value)}/> </h3>
         <h3 style = {{color: "white"}}>Type: {annotation?.type}</h3>
         <p style = {{color: "white"}}>
-        Comment: <input type = "text" placeholder="Enter comment" value = {annotation?.comment} onChange = {(e) => updateComment(e.target.value)}/>
+        Comment: <input type = "text"  placeholder="Enter comment" value = {annotation?.comment} onChange = {(e) => updateComment(e.target.value)}/>
         </p>
         {/* <p style = {{color: "white"}}> */}
             {/* {annotation?.points.map((point) => { */}
