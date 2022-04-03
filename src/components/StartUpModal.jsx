41fd6194999c08e703def2ec029ba216ff47a639
NickDempseyDev <nickdempseydev@gmail.com>
2022-04-03 10:54:55 +0100

    turned off modal beofre merging as not complete

--- src/components/StartUpModal.jsx
+++ src/components/StartUpModal.jsx
@@ -3,6 +3,9 @@ import Modal from "react-modal/lib/components/Modal";
 import "../styles/Modals.css";
 
 const customStyles = {
+	overlay: {
+		zIndex: "11"
+	},
 	content: {
 		top: "50%",
 		left: "50%",
@@ -11,7 +14,6 @@ const customStyles = {
 		marginRight: "-50%",
 		transform: "translate(-50%, -50%)",
 		borderLeft: "0.5em solid #0f62fe",
-		zIndex: "9",
 		padding: "0",
 		margin: "0",
 		width: "50vw",
@@ -26,7 +28,7 @@ Modal.setAppElement("#root");
 const StartUpModal = ({ isOpen }) => {
 	
 	return (
-		<Modal style={customStyles} contentLabel="Start Modal" isOpen={isOpen}>
+		<Modal style={customStyles} contentLabel="Start Modal" isOpen={false}>
 			<div className="modal__title">
 				Create a New Project or Open an Existing One
 			</div>
