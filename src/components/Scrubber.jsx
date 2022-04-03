7847bf3a7532da050ac5366ef7ed5d0b8ef4c0d0
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-04-03 23:35:20 +0100

    Gonna try making a slider from scratch since the inbuild one is difficult.

--- src/components/Scrubber.jsx
+++ src/components/Scrubber.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Scrubber.css";
 
-const Scrubber = () => {
+const Scrubber = ({ videoTime }) => {
 	return (
 		<div className="scrubberContainer">
-			<input className="scrubber" type="range" min="0" max="100" step="0.1"></input> 
+			<input className="scrubber" type="range" min="0" max="100" step="0.1" defaultValue={videoTime}></input> 
 		</div>
 	);
 };
