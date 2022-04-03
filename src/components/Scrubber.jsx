aa55592786d952cd7f9375385c74dd185f308e61
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-04-04 00:55:43 +0100

    Added scrubbing bar.

--- src/components/Scrubber.jsx
+++ src/components/Scrubber.jsx
@@ -1,16 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../styles/Scrubber.css";
 
-const Scrubber = ({ currentPercent }) => {
+const Scrubber = ({ currentPercent, gotoPercent }) => {
 	const [progressStyle, setProgressStyle] = useState({"width": "0%"});
+	const [tempStyle, setTempStyle] = useState({"width": "0%"});
+	const divElement = useRef(null);
 
 	useEffect(() => {
 		setProgressStyle({"width": currentPercent+"%"});
 	}, [currentPercent]);
 
+	// Function to convert x coordinate to percent.
+	const coordToPercent = (x) => {
+		const barWidth = divElement.current.getBoundingClientRect().width;
+		const barStart = divElement.current.getBoundingClientRect().left;
+		return ((x-barStart)/(barWidth))*100;
+	}
+
+	const mouseMove = (e) => {
+		const currentPercent = coordToPercent(e.clientX);
+		setTempStyle({"width": currentPercent+"%"});
+	}
+
+	const mouseLeave = (e) => {
+		setTempStyle({"width": "0%"});
+	}
+
+	const mouseDown = (e) => {
+		const currentPercent = coordToPercent(e.clientX);
+		console.log(currentPercent);
+		gotoPercent(currentPercent);
+	}
+
 	return (
-		<div className="scrubberContainer">
+		<div onMouseMove={mouseMove} onMouseLeave={mouseLeave} onMouseDown={mouseDown} ref={divElement} className="scrubberContainer">
 			<div style={progressStyle} className="progressBar"></div>
+			<div style={tempStyle} className="potentialBar"></div>
 		</div>
 	);
 };
