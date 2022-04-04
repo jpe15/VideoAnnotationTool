6a9df2a66aa0c4bd045be0b1344f17ac6560c2a0
Naveen15012003 <71087983+Naveen15012003@users.noreply.github.com>
2022-04-04 14:05:18 +0500

    JSON export enhanced
    Removed some extra comments

--- public/export.js
+++ public/export.js
@@ -39,12 +39,8 @@ async function exportData(projName, data, metadata, videoPath) {
         filename = filename.split('.')[0]
 
         let Json_obj = JSON.parse('{"projectName": "", "projectPath": "", "VideoPath": "", "annotatedFrames": "" }')
-        //let AnnotatedFrames =   JSON.parse('{"metadata": "", "annotations": "" }')
-        
-        
-       // let metaData =JSON.parse('{"metadata": ""}') ;
+
         let metadata_obj = JSON.parse('{"metadata":{"imageName": "" ,"timestamp": "", "comment": ""}}')
-        //metadata_obj["metadata"] =metadata_obj;
 
 
         let annotation = JSON.parse('{"annotation" : ""}') ;
@@ -53,9 +49,6 @@ async function exportData(projName, data, metadata, videoPath) {
 
         let aDataFaram = [metadata_obj,annotation];
         Json_obj["annotatedFrames"] = aDataFaram
-        
-        
-        //Json_obj["annotatedFrames"]["metadata"] = JSON.parse(metadata_obj)
 
        //let unique = [... new Set(data.map(i => i.timestamp))];
 
@@ -67,45 +60,7 @@ async function exportData(projName, data, metadata, videoPath) {
         Json_obj["VideoPath"] = videoPath
        
         Json_obj["annotatedFrames"]["annotations"] = data
-        //let d = Json_obj["annotatedFrames"]
-       // Json_obj["annotatedFrames"]["metadata"]["timestamp"] = data[0]["timestamp"]
-
-       // da = Array.from(d)
-       // console.log(d);
-        //Json_obj["annotatedFrames"] = da
-
-        //Json_obj["annotatedFrames"]["metadata"] = metadata      //<-- this metadata gives undefined idk why ?
-
-        /*
-        FORMAT OF JSON:
-        {
-            "projectName": "name of the project",
-            "videoPath": "path to the video file",
-            "annotatedFrames": [
-            {
-                "metadata": {
-                    "imagePath": "path to the image file",
-                    "timeStamp": "timestamp of frame in seconds (float)",
-                    "comment": "any comment for this frame"
-                },
-                "annotations": [
-                    {
-                        "type": "POLYGON",
-                        "points": [["x1", "y1"], ["x2", "y2"], ["x3", "y3"]],
-                        "label": "the user provided label",
-                        "comment": "the user provided comment"
-                    },
-                    {
-                        "type": "BOX",
-                        "points": [["x_left_top", "y_left_top"], ["x_right_bottom", "y_right_bottom"]],
-                        "label": "the user provided label",
-                        "comment": "the user provided comment"
-                    }
-                ]
-            },
-        }
-        */
-
+        
         //function responsible for writing data on file
         //path will be resolved through path module the JSON will be converted to string and written on file using JSON.stringigy
         fs.writeFile(path.resolve(directory, `${filename}.json`), JSON.stringify(Json_obj), (err) => {
